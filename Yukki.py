5a735306c0ab6783f40fea08c3d5b31af6f6ab44
UserXTester <71839916+UserXTester@users.noreply.github.com>
2021-09-16 22:17:43 +0700

    Update Yukki.py
    beta10.8

--- Yukki.py
+++ Yukki.py
@@ -1,4 +1,4 @@
-# v2.0.11 beta10.7
+# v2.0.11 beta10.8
 
 import os
 import sys
@@ -90,7 +90,7 @@ async def start_yukki():
     global edk
 
 
-    print("bot v2.0.11 beta10.7 is starting...")
+    print("bot v2.0.11 beta10.8 is starting...")
     print("")
     if smex:
         session_name = str(smex)
@@ -1123,9 +1123,9 @@ async def setname(event):
 @ddk.on(events.NewMessage(incoming=True, pattern=r"\.dm ?(.*)"))
 
 async def dm(e):
-    if event.sender_id in SMEX_USERS:
-        if not e.out and not is_fullsudo(e.sender_id):
-            return await event.reply(e, "`This Command is Full Sudo Restricted..`")
+    if e.sender_id in SMEX_USERS:
+        # if not e.out and not is_fullsudo(e.sender_id):
+            # return await e.reply(e, "`This Command is Full Sudo Restricted..`")
         if len(e.text) > 3:
             if not e.text[3] == " ":  # weird fix
                 return
@@ -1134,21 +1134,21 @@ async def dm(e):
         try:
             chat_id = await get_user_id(c[0])
         except Exception as ex:
-            return await event.reply(e, "`" + str(ex) + "`")
+            return await e.reply(e, "`" + str(ex) + "`")
         msg = ""
         masg = await e.get_reply_message()
         if e.reply_to_msg_id:
             await e.client.send_message(chat_id, masg)
-            await event.reply(e, "`⚜️Message Delivered!`")
+            await e.reply(e, "`⚜️Message Delivered!`")
         for i in c[1:]:
             msg += i + " "
         if msg == "":
             return
         try:
             await e.client.send_message(chat_id, msg)
-            await event.reply(e, "`⚜️Message Delivered!⚜️`")
+            await e.reply(e, "`⚜️Message Delivered!⚜️`")
         except BaseException:
-            await event.reply(e, f"Read Usage : `.help dm`")
+            await e.reply(e, f"Read Usage : `.help dm`")
 
 
 
@@ -1256,7 +1256,7 @@ async def help(e):
 
 For more help regarding usage of plugins type plugins name
 
-🤖 𝘽𝙤𝙩 𝙑𝙚𝙧𝙨𝙞𝙤𝙣: <code>v2.0.11 beta10.7</code>"""
+🤖 𝘽𝙤𝙩 𝙑𝙚𝙧𝙨𝙞𝙤𝙣: <code>v2.0.11 beta10.8</code>"""
        await e.reply(text, parse_mode='html', link_preview=None )
 
         
@@ -1273,7 +1273,7 @@ text = """
 
 print(text)
 print("")
-print("SMEX! Yukki Mult1 5p4mX UBot v2.0.11 beta10.7 Started Sucessfully.")
+print("SMEX! Yukki Mult1 5p4mX UBot v2.0.11 beta10.8 Started Sucessfully.")
 if len(sys.argv) not in (1, 3, 4):
     try:
         idk.disconnect()
