2ecd0cd170213e603e7e0fb8fc42c0d9db631240
UserXTester <71839916+UserXTester@users.noreply.github.com>
2021-09-17 12:02:40 +0700

    yukki.py
    v3.1.1.10 beta15.3
    - fixxxxx

--- Yukki.py
+++ Yukki.py
@@ -1,4 +1,4 @@
-# v3.1.1.10 beta15.2
+# v3.1.1.10 beta15.3
 
 import os
 import sys
@@ -109,7 +109,7 @@ async def start_yukki():
     global edk
 
 
-    print("bot v3.1.1.10 beta15.2 is starting...")
+    print("bot v3.1.1.10 beta15.3 is starting...")
     print("")
     if smex:
         session_name = str(smex)
@@ -1070,6 +1070,17 @@ async def update_username(username):
 # STRINGS
 STAT_INDICATION = "`Collecting stats, Please wait....`"
 
+# Functions
+def user_full_name(user):
+    names = [user.first_name, user.last_name]
+    names = [i for i in list(names) if i]
+    return " ".join(names)
+
+
+def inline_mention(user):
+    full_name = user_full_name(user) or "No Name"
+    return f"[{full_name}](tg://user?id={user.id})"
+
 @idk.on(events.NewMessage(incoming=True, pattern=r"^\.stats$"))
 @ydk.on(events.NewMessage(incoming=True, pattern=r"^\.stats$"))
 @wdk.on(events.NewMessage(incoming=True, pattern=r"^\.stats$"))
@@ -1484,7 +1495,7 @@ async def help(e):
 
 For more help regarding usage of plugins type plugins name
 
-🤖 𝘽𝙤𝙩 𝙑𝙚𝙧𝙨𝙞𝙤𝙣\t: <code>v3.1.1.10 beta15.2</code>
+🤖 𝘽𝙤𝙩 𝙑𝙚𝙧𝙨𝙞𝙤𝙣\t: <code>v3.1.1.10 beta15.3</code>
 🤖 𝘽𝙤𝙩 𝙏𝙮𝙥𝙚\t\t: <code>YKX</code>"""
        await e.reply(text, parse_mode='html', link_preview=None )
 
@@ -1502,7 +1513,7 @@ text = """
 
 print(text)
 print("")
-print("SMEX! Yukki Mult1 5p4mX UBot v3.1.1.10 beta15.2 Started Sucessfully.")
+print("SMEX! Yukki Mult1 5p4mX UBot v3.1.1.10 beta15.3 Started Sucessfully.")
 if len(sys.argv) not in (1, 3, 4):
     try:
         idk.disconnect()
